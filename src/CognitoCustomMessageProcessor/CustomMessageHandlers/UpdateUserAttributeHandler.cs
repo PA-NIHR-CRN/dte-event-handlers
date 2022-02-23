04a21bca91fd5f276343db9b0c1f226244b2cf5e
atif rehman <atif.rehman@thetrainline.com>
2022-02-23 12:55:42 +0000

    Adding UpdateUserAttributeHandler

---
+++ src/CognitoCustomMessageProcessor/CustomMessageHandlers/UpdateUserAttributeHandler.cs
@@ -0,0 +1,44 @@
+using System.Threading.Tasks;
+using System.Web;
+using Dte.Common.Lambda.Contracts;
+using Dte.Common.Lambda.Events;
+using CognitoCustomMessageProcessor.Content;
+using CognitoCustomMessageProcessor.Contracts;
+using CognitoCustomMessageProcessor.CustomMessages;
+using CognitoCustomMessageProcessor.Settings;
+using Microsoft.Extensions.Logging;
+
+namespace CognitoCustomMessageProcessor.CustomMessageHandlers
+{
+    public class UpdateUserAttributeHandler : IHandler<CustomMessageUpdateUserAttribute, CognitoCustomMessageEvent>
+    {
+        private readonly ILinkBuilder _linkBuilder;
+        private readonly AppSettings _appSettings;
+        private readonly ILogger<UpdateUserAttributeHandler> _logger;
+
+        public UpdateUserAttributeHandler(ILinkBuilder linkBuilder, AppSettings appSettings, ILogger<UpdateUserAttributeHandler> logger)
+        {
+            _linkBuilder = linkBuilder;
+            _appSettings = appSettings;
+            _logger = logger;
+        }
+        
+        public async Task<CognitoCustomMessageEvent> HandleAsync(CustomMessageUpdateUserAttribute source)
+        {
+            var requestCodeParameter = source.Request.CodeParameter;
+            var userAttributesEmail = HttpUtility.UrlEncode(source.Request.UserAttributes.Email);
+            
+            var links = _linkBuilder
+                .AddLink("VerifyEmail", $"{_appSettings.DteWebBaseUrl}verifyemail", "0", userAttributesEmail)
+                .Build();
+            
+            source.Response.EmailSubject = $"hi, from UpdateUserAttribute";
+            source.Response.EmailMessage = CustomMessageEmail.GetCustomMessageHtml()
+                .Replace("###TEXT_REPLACE1###", "You have changed your account details.")
+                .Replace("###TEXT_REPLACE2###", "Please click the link below to verify your account")
+                .Replace("###LINK_REPLACE###", links);
+            
+            return await Task.FromResult(source);
+        }
+    }
+}
